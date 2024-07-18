52659dea1e895b6c55731f99953b8d0cb4472a75
Fernando <info_fotografia@icloud.com>
2024-07-18 21:42:54 +0200

    Creación de los casos de la App de listado de personas

--- src/ListadoPersonasApp.java
+++ src/ListadoPersonasApp.java
@@ -15,8 +15,12 @@ public class ListadoPersonasApp {
         var salir = false;
         while (!salir){
             mostrarMenu();
+            try {
+                salir = ejecutarOperacion(consola, personas);
+            } catch (Exception e){
+                System.out.println("Ocurrió un error: " + e.getMessage());
+            }
             System.out.println();
-            break;
         }
     }
 
@@ -25,9 +29,43 @@ public class ListadoPersonasApp {
                 **** Listado Personas App ****
                 1. Agregar
                 2. Listar
-                3. Eliminar
-                4. Salir
+                3. Salir
                 """);
         System.out.println("Proporciona la opción? ");
+    }
+
+    private static boolean ejecutarOperacion(Scanner consola, List<Persona> personas){
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+
+        //Revisamos la opción selecionada
+        switch (opcion){
+            case 1 -> { //Agregar persona a la lista
+                System.out.print("Proporciona el nombre: ");
+                var nombre = consola.nextLine();
+                System.out.print("Proporciona el telefono: ");
+                var tel = consola.nextLine();
+                System.out.print("Proporciona el email: ");
+                var email = consola.nextLine();
+
+                //Crear el objeto Persona
+                var persona = new Persona(nombre, tel, email);
+                //Lo agregamos a la lista
+                personas.add(persona);
+                System.out.println("La lista tiene: " + personas.size() + " elementos");
+
+            }//Fin caso 1
+            case 2 -> {//Listar las personas
+                System.out.println("Listado de personas: ");
+                //Mejora usando lambda y metodo de referencia
+                personas.forEach(System.out::println);
+            }
+            case 3 -> {//Salimos del ciclo
+                System.out.println("Hasta pronto... ");
+                salir = true;
+            }
+            default -> System.out.println("Opción erronea: " + opcion);
+        }//Fin switch
+        return salir;
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -7,13 +7,13 @@ public class Persona {
     private static int numeroPersonas = 0;
 
     //Constructor Vacio
-    public Persona() {
+    public Persona(String nombre, String tel) {
         this.id = ++Persona.numeroPersonas;
     }
 
     //Constructor con argumentos
     public Persona(String nombre, String tel, String email) {
-        this();
+        this(nombre, tel);
         this.nombre = nombre;
         this.tel = tel;
         this.email = email;
